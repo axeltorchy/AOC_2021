ad503cc55996f6380b7849b0dda7029226d29759
Axel Torchy <axel@wizzilab.com>
2022-12-15 19:43:23 +0100

    Clean prints day 6 solution

--- code/day6.py
+++ code/day6.py
@@ -19,13 +19,13 @@ N_days = 256
 def number_of_fish(fish):
     return sum(fish)
 
-print(number_of_fish(fish), fish)
+#print(number_of_fish(fish), fish)
 for i in range(N_days):
     N_births = fish[0]
     for j in range(1, delay + additional_delay):
         fish[j - 1] = fish[j]
     fish[delay + additional_delay - 1] = N_births
     fish[delay - 1] += N_births
-    print(number_of_fish(fish), fish)
+    #print(number_of_fish(fish), fish)
 
 print(f"Total number of fish after {N_days}: {number_of_fish(fish)}")
